7e129ec8d5d2f094574c64ae1ebe14af506d92bb
Jacob Hayes <jahayes5@my.waketech.edu>
2025-05-06 16:59:29 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -28,7 +28,27 @@ function fetchGradeData() {
 function populateGradebook(data) {
      // This function will take the fetched grade data and populate the table
      console.log("Populating gradebook with data:", data);
-}
+     let tableElm = document.getElementByID("gradebook"); // Get the gradebook table element
+         data.foreach(function(assignemnt){ // For each row of data we're passed in
+             let row = document.createElement("tr"); // create a table row element
+             let colunms = []; // Handy place to stick the column of information
+             columns.name = document.createElement('td'); // The first column's table data will be the name
+             columns.name.appendChild(
+                 // Concatenate the full name: "last_name, first_name"
+                 document.createTextNode(assignment.last_name + ", " + assignment.first_name)
+             );
+             columns.grade = document.createElement('td'); // second column will be the grade
+             columns.grade.appendChild(
+                 // Just put the name in the text, you could be fancy and figure out the letter grade here
+                 // with either a bunch of conditions, or a JavaScript "switch" statement
+                 document.createTextNode(assignment.total_grade)
+            );
+             // Add the table data columns to the table row
+             row.appendChild(columns.name);
+             row.appemdChild(columns.grade);
+             // Add the row to the table itself to make the data visible
+             tableElm.appendChild(row);
+  });
 
 // TODO REMOVE THIS
 // Call the stubs to demonstrate the workflow
