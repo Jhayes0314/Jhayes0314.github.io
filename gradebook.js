8ce9a22940d1ce42ab756702729460c156facab6
Jacob Hayes <jahayes5@my.waketech.edu>
2025-05-07 17:44:52 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,16 +1,16 @@
-// TODO: Fetch data grom the PostgreSQL database (to be implemented later)
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
 function fetchGradeData() {
     // This function will query the PostgreSQL database and return grade data
     console.log("Feteching grade data...");
     //Create a new request for the HTTP data
-    let xhr = new XMLHttp();
+    let xhr = new XMLHttpRequest();
     // This is the address on the machine we're asking for data
     let apiroute = "/api/grades";
     //When the request changes status we run this anonymous function
     xhr.onreadystatechange = function(){
     let results;
-      //Check if we're done
-      if(xhr.readystate === xhr.done){
+      // Check if we're done
+      if(xhr.readyState === xhr.DONE){
             // Check if we're successful
             if(xhr.status !== 200){
                 console.error(`Could not get grades.
